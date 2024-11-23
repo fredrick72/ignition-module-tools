c887aaabea1e5758043c6b169fe82cabbce622dd
Eric Fredericksen <eric.e.fredericksen@gmail.com>
2024-11-23 09:42:29 -0700

    Performance Tracker Project Added

---
+++ generator/generator-cli/performance-tracker/client/src/main/java/com/eleconsulting/performandtracker/client/PerformanceTrackerClientHook.java
@@ -0,0 +1,12 @@
+package com.eleconsulting.performandtracker.client;
+
+import com.inductiveautomation.vision.api.client.AbstractClientModuleHook;
+
+/**
+ * Client Hook for projects which target Vision
+ *
+ * @since <DATE>
+ */
+public class PerformanceTrackerClientHook extends AbstractClientModuleHook {
+    // override methods as required
+}
---
+++ generator/generator-cli/performance-tracker/common/src/main/java/com/eleconsulting/performandtracker/common/PerformanceTrackerModule.java
@@ -0,0 +1,6 @@
+package com.eleconsulting.performandtracker.common;
+
+public class PerformanceTrackerModule {
+    public static final String MODULE_ID = "com.eleconsulting.performandtracker.PerformanceTracker";
+
+}
